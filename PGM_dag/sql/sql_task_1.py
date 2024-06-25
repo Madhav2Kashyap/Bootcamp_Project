e547b7fd45f4e8596fb67882b2d9737e0513a836
Madhav2Kashyap <126601037+Madhav2Kashyap@users.noreply.github.com>
2024-06-25 22:38:09 +0530

    Delete PGM_dag directory

--- PGM_dag/sql/sql_task_1.py
+++
@@ -1,9 +0,0 @@
-compare_stg_tbl_task1='''
-INSERT INTO employee_data_dim (name, age, emp_id)
-SELECT name, age, emp_id
-FROM employee_data_stg
-EXCEPT
-SELECT name, age, emp_id
-FROM employee_data_dim;
-
-'''
